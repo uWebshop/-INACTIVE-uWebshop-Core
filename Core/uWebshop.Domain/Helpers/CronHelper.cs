0d695c9057bafc347038152993efcb296d48ab86
mark <mark@mindbus.nl>
2015-06-01 17:36:02 +0200

    CronHelper: included NCronTab source

---
+++ Core/uWebshop.Domain/Helpers/CronHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uWebshop.Domain.Helpers
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class CronHelper
+	{
+		/// <summary>
+		/// Generates the date time instances from a crontab expression starting now, until the end time or one year in advance if not specified.
+		/// </summary>
+		/// <param name="crontabExpression">The crontab expression.</param>
+		/// <param name="endTime">The end time.</param>
+		public static IEnumerable<DateTime> GenerateDateTimeInstancesFromCrontabExpressionStartingNow(string crontabExpression, DateTime? endTime = null)
+		{
+			return CrontabSchedule.CrontabSchedule.Parse(crontabExpression).GetNextOccurrences(DateTime.Now, endTime ?? DateTime.Now.AddYears(1));
+		}
+	}
+}
---
+++ Tests/uWebshop.Test/Domain/Helper classes/CronHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using uWebshop.Domain.Helpers;
+
+namespace uWebshop.Test.Domain.Helper_classes
+{
+	[TestFixture]
+	public class CronHelperTests
+	{
+		[Test]
+		public void ManualCheckOfCronHelper()
+		{
+			// functionality of NCronTab is assumed tested
+			var a = CronHelper.GenerateDateTimeInstancesFromCrontabExpressionStartingNow("0 15 * jan,feb mon,tue");
+			foreach (var dateTime in a)
+			{
+				Console.WriteLine(dateTime);
+			}
+		}
+	}
+}
